f0b1e2d3636e145bde19d7eb198adece174d67b8
Niaj Ahammed Shuvo <shuvoniaj@gmail.com>
2026-08-06 15:39:02 +0600

    c

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const inputBar = document.getElementById("input-bar");
+const searchBtn = document.getElementById("search-btn");
+
+function fetchCocktail () {
+  let URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail`;
+}
